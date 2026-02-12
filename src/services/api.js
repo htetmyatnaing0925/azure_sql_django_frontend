6f0a36dbcfcbd76dc093aa6b21f5b8a04d0cfe67
htetmyatnaing0925 <htetmyatnaing0925@gmail.com>
2026-02-13 01:42:09 +0700

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'naing-backend-hubzfteqgpbre0dq.southeastasia-01.azurewebsites.net/api',
+  baseURL:  'naing-backend-hubzfteqgpbre0dq.southeastasia-01.azurewebsites.net/api',
   // baseURL: 'http://127.0.0.1:8000/api', 
   headers: {
     'Content-Type': 'application/json',
